c8ac0224d88db3199a223fe44828e922949f1dfd
ItzFrxn <255025318+ItzFrxn@users.noreply.github.com>
2026-07-24 15:50:44 -0500

    ThemeToggle updated, now saves the theme used

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+let darkmode = localStorage.getItem("darkmode");
 const themeToggle = document.querySelector(".btn");
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
+const enableDarkMode = () => {
+  document.body.classList.add("dark-theme");
+  localStorage.setItem("darkmode", "active");
 
-  themeToggle.querySelector("span:nth-child(1)").classList.toggle("active");
-  themeToggle.querySelector("span:nth-child(2)").classList.toggle("active");
+  themeToggle.querySelector("span:nth-child(1)").classList.remove("active");
+  themeToggle.querySelector("span:nth-child(2)").classList.add("active");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark-theme");
+  localStorage.setItem("darkmode", null);
+
+  themeToggle.querySelector("span:nth-child(1)").classList.add("active");
+  themeToggle.querySelector("span:nth-child(2)").classList.remove("active");
+};
+
+if (darkmode === "active") enableDarkMode();
+
+themeToggle.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode();
 });
